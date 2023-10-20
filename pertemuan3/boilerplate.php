ccba8c809a2fc3463ac63324bf42d337dce6cc39
sh1z1a <h4sn44ziz4h@gmail.com>
2023-10-20 08:42:12 +0700

    tugas class animals

--- pertemuan3/boilerplate.php
+++ pertemuan3/boilerplate.php
@@ -28,14 +28,22 @@ class Animal
     public function store($data)
     {
         # gunakan method array_push untuk menambahkan data baru
-        array_push($this->animals, $data); #$data untuk menerima data yang baru ditambahkan 
+        array_push($this->animals, $data); # $data untuk menerima data yang baru ditambahkan 
     }
 
     # method update - mengupdate hewan
     # parameter: index dan hewan baru
     public function update($index, $data)
     {
-        $this->animals[$index] = $data;
+        // $this->animals[$index] = $data;
+
+        // cek apakah index valid
+        if(!isset($this->animals[index])){
+            throw new Exception("Index tidak keluar");
+        }
+
+        $this->animals[$index] = $animal;
+        return $this;
     }
 
     # method delete - menghapus hewan
@@ -52,21 +60,21 @@ class Animal
 # kirimkan data hewan (array) ke constructor
 $animal = new Animal(["Ayam", "Ikan"]);
 
-echo "Index - Menampilkan seluruh hewan <br>";
+echo "Index - Menampilkan seluruh data hewan <br>";
 $animal->index();
 echo "<br>";
 
-echo "Store - Menambahkan hewan baru <br>";
+echo "Store - Menambahkan data hewan baru <br>";
 $animal->store('burung');
 $animal->index();
 echo "<br>";
 
-echo "Update - Mengupdate hewan <br>";
+echo "Update - Mengupdate data hewan <br>";
 $animal->update(0, 'Kucing Anggora');
 $animal->index();
 echo "<br>";
 
-echo "Destroy - Menghapus hewan <br>";
+echo "Destroy - Menghapus data hewan <br>";
 $animal->destroy(1);
 $animal->index();
 echo "<br>";
