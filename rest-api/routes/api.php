b5c33d1435d95459ad095c58d102cddf5755bd29
sh1z1a <h4sn44ziz4h@gmail.com>
2024-01-24 22:15:13 +0700

    Update api.php

--- rest-api/routes/api.php
+++ rest-api/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController;
 use App\Http\Controllers\StudentController;
+use App\Models\Student;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,10 +17,12 @@ use App\Http\Controllers\StudentController;
 |
 */
 
+// sanctum itu buat autentifikasi jadi memastikan kalau data kita aman
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Start Route for animals
 // method get
 Route::get('/animals', [AnimalController::class, 'index']);
 
@@ -31,10 +34,24 @@ Route::put('/animals/{id}', [AnimalController::class, 'update']);
 
 // method delete
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
+// end route
 
-
-// student get all
+// Start Route for students
+// method get
 Route::get('/students', [StudentController::class, 'index']);
 
-// menambahkan data student
+// method post
 Route::post('/students', [StudentController::class, 'store']);
+
+// method put
+Route::put('/students/{id}', [StudentController::class, 'update']);
+
+// method delete
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+// end route
+
+/**
+ * Nama     : Hasna Azizah 
+ * Kelas    : SE03 / TI02
+ * NIM      : 0110222235
+ */
